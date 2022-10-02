8cf94f77b84b3faa65cc604d094fc307bf8b07b4
KazTzy <113179889+IyannAja@users.noreply.github.com>
2022-10-02 09:52:37 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,13 +14,13 @@ let handler = async(m, { conn }) => {
     surface : 404,
     message: `Nih My Mastah :3`, 
     orderTitle: ``,
-    thumbnail: await (await fetch('https://telegra.ph/file/7501d485189f34dc429a7.jpg')).buffer(), //Gambarnye
+    thumbnail: await (await fetch('https://telegra.ph/file/4afc10ce016e4a7689ed3.jpg')).buffer(), //Gambarnye
     sellerJid: '0@s.whatsapp.net' 
     }
     }
     }
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+    let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/4afc10ce016e4a7689ed3.jpg")
 	conn.p[id] = [
 	await conn.sendKontak(m.chat, kontak2, ftroli, { contextInfo: { externalAdReply :{ 
      showAdAttribution: true, 
@@ -32,8 +32,8 @@ let handler = async(m, { conn }) => {
     contextInfo: { //forwardingScore: 99999, isForwarded: true,
         externalAdReply: { //showAdAttribution: true,
         	sourceUrl: 'https://vt.tiktok.com/ZSRRmS8vh/',
-            title: 'Jangan Bully Zivfurr Dia Kang Kopas <3',
-            body: 'Follow Tiktok Pacar Saya Bang',
+            title: 'Iyann Cuma Recode <3',
+            body: 'Yang Spam Kek Anjg',
           thumbnail: await (await fetch(pp)).buffer()
         }
      }
